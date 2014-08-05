3c120f0adb80c081e1d458f7cb22bd12e548c567
omphalos <omph.alos@yahoo.com>
2014-08-05 11:43:30 -0500

    Update options object to use proto inheritance.

--- test/test.i.js
+++ test/test.i.js
@@ -12,7 +12,6 @@ Error.stackTraceLimit = 15
 
 var cacheFile = __dirname + '/.inductive.cache.json'
 if(fs.existsSync(cacheFile)) fs.unlinkSync(cacheFile)
-
 require('./testTypes.i.js')
 require('./testSolves.i.js')
 require('./testFiles.i.js')
