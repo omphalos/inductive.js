0e1e6501f39bce624d83c3f54f85867b3f447a74
omphalos <omph.alos@yahoo.com>
2015-06-05 05:08:31 -0500

    Update README

--- examples/helloworld/helloworld.i.js
+++ examples/helloworld/helloworld.i.js
@@ -2,14 +2,16 @@
 require('../../lib/inductive.js').globals()
 
 // Specify our program:
-run(
-  specify('helloWorld',
-    givenNoArgs(
-      mock('console.log',
-        verify('hello world'))),
-    use(
-      'console.log',
-      value('hello world'))))
+var helloWorld = specify('helloWorld',
+  givenNoArgs(
+    shouldReturn(undefined),
+    mock('console.log',
+      verify('hello world'))),
+  use(
+    'console.log',
+    value('hello world')))
+
+run(helloWorld)
 
 // Generate the .js file:
 saveAll()
